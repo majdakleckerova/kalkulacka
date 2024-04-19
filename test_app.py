05fc0b817c7bad0805dda4d442fe29f9787dbc09
Pavel Klas <94009466+KLASlK@users.noreply.github.com>
2024-04-19 18:43:09 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,3 +1,7 @@
 from vetevicka import funkce_scitani
+import funkce_nasobeni
 def test_scitani():
     assert funkce_scitani.scitani(4, 2) == 6
+
+def test_nasobeni():
+    assert funkce_nasobeni(4, 6) == 13
